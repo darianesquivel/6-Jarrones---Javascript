9c50b896e8b6243f7cd30a95933353f02d38c105
Darian Esquivel <darianesquivelf@gmail.com>
2021-08-31 19:49:21 -0300

    Iniciando repo

---
+++ js/main.js
@@ -0,0 +1,111 @@
+document
+  .querySelector("#formIngreso")
+  .addEventListener("submit", guardarIngreso);
+
+document.querySelector("#formGasto").addEventListener("submit", guardarGasto);
+
+window.addEventListener("load", cargarPagina); // al cargar la pagina
+
+let jarrones = [
+  { nombre: "Necesidades", valorTotalDisponible: 0, porcentaje: 55 },
+  { nombre: "Educacion", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Ahorro", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Diversion", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Inversion", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Dar", valorTotalDisponible: 0, porcentaje: 5 },
+];
+
+let todoIngreso = []; // Array con ingresos y descripciones
+let todoGasto = []; // Array con gastos y descripciones
+let sumaJarroNecesidades = 0;
+let sumaJarroEducacion = 0;
+
+function cargarPagina() {
+  jarrones.map(imprimirOption);
+  jarrones.map(imprimirJarrones);
+}
+
+function imprimirOption(valor, indice) {
+  jarron.innerHTML += `<option value="${indice}"> ${valor.nombre} </option>`;
+}
+
+function imprimirJarrones(valor, indice) {
+  contenedorJarrones.innerHTML += ` <div class="contenedorJarron">
+<div class="tituloJarron">
+  <h3>${valor.nombre}</h3>
+</div>
+<div class="montoJarron">
+  <h3 id="valor${indice}">$${valor.valorTotalDisponible.toFixed(2)}</h3>
+</div>
+<div class="botonJarron">
+  <div>
+    <h3>${valor.porcentaje}%</h3>
+  </div>
+</div>
+</div> `;
+}
+
+function guardarIngreso(e) {
+  e.preventDefault(); // hace que no se refresque la pagina cada vez que apretamos el submit del formulario
+
+  let montoIngreso = Number(document.querySelector("#montoIngreso").value);
+  let descIngreso = document.querySelector("#descripcionIngreso").value;
+
+  todoIngreso.unshift({ montoIngreso, descIngreso });
+
+  jarrones.map((valor, indice) => {
+    valor.valorTotalDisponible =
+      valor.valorTotalDisponible + (montoIngreso / 100) * valor.porcentaje;
+    let idvalor = document.querySelector(`#valor${indice}`);
+    idvalor.innerHTML = `$ ${valor.valorTotalDisponible.toFixed(2)}`;
+  });
+
+  imprimirIngresos();
+}
+
+function guardarGasto(e) {
+  e.preventDefault(); // hace que no se refresque la pagina cada vez que apretamos el submit del formulario
+
+  let montoGasto = Number(document.querySelector("#montoGasto").value);
+  let descGasto = document.querySelector("#descripcionGasto").value;
+  let jarron = document.querySelector("#jarron").value;
+
+  todoGasto.unshift({ montoGasto, descGasto, jarron });
+  console.log(todoGasto);
+
+  jarrones[jarron].valorTotalDisponible =
+    jarrones[jarron].valorTotalDisponible - montoGasto;
+
+  let idvalor = document.querySelector(`#valor${jarron}`);
+  idvalor.innerHTML = `$ ${jarrones[jarron].valorTotalDisponible.toFixed(2)}`;
+
+  imprimirGastos();
+}
+
+function imprimirGastos() {
+  let htmlListaGasto = "";
+  todoGasto.map((valor, indice) => {
+    if (indice > 9) {
+      return;
+    }
+    htmlListaGasto += `<p> Descripcion : ${valor.descGasto}  Monto: $${
+      valor.montoGasto
+    }  Jarron: ${jarrones[valor.jarron].nombre}</p> `;
+  });
+
+  listaGasto.innerHTML = htmlListaGasto;
+}
+
+function imprimirIngresos() {
+  let htmlListaIngreso = "";
+
+  todoIngreso.map((valor, indice) => {
+    if (indice > 9) {
+      return;
+    }
+
+    htmlListaIngreso += `<p> Descripcion : ${valor.descIngreso}  Monto: $${valor.montoIngreso} `;
+  });
+
+  listaIngreso.innerHTML = htmlListaIngreso;
+}
