e3d1846a58c6c468694563f426de84d4e695b2f3
Darian Esquivel <darianesquivelf@gmail.com>
2021-09-19 10:27:47 -0300

    todo arrowf

--- js/main.js
+++ js/main.js
@@ -1,134 +1,14 @@
-document
-  .querySelector("#formIngreso")
-  .addEventListener("submit", guardarIngreso); // Detecta click boton de ingresos y ejecuta la funcion guardarIngreso
-
-document.querySelector("#formGasto").addEventListener("submit", guardarGasto); // Detecta click boton de gastos y ejecuta la funcion guardarGasto
-
-window.addEventListener("load", cargarPagina); // Cuando carga la pagina, ejecuta las funciones imprimirOption y imprimirJarrones
-
-let jarrones = [
-  { nombre: "Necesidades", valorTotalDisponible: 0, porcentaje: 55 },
-  { nombre: "Educacion", valorTotalDisponible: 0, porcentaje: 10 },
-  { nombre: "Ahorro", valorTotalDisponible: 0, porcentaje: 10 },
-  { nombre: "Diversion", valorTotalDisponible: 0, porcentaje: 10 },
-  { nombre: "Inversion", valorTotalDisponible: 0, porcentaje: 10 },
-  { nombre: "Dar", valorTotalDisponible: 0, porcentaje: 5 },
-];
-
-let todoIngreso = []; // Array con ingresos y descripciones
-let todoGasto = []; // Array con gastos y descripciones
-
-function cargarPagina() {
-  jarronesLocalStorage();
-  jarrones.map(imprimirOption);
-  jarrones.map(imprimirJarrones);
-  imprimirIngresoLocalStorage();
-  imprimirGastosLocalStorage();
-}
-
-function jarronesLocalStorage() {
-  jarrones = JSON.parse(localStorage.getItem("jarrones"));
-
-  if (jarrones === null) {
-    jarrones = [
-      { nombre: "Necesidades", valorTotalDisponible: 0, porcentaje: 55 },
-      { nombre: "Educacion", valorTotalDisponible: 0, porcentaje: 10 },
-      { nombre: "Ahorro", valorTotalDisponible: 0, porcentaje: 10 },
-      { nombre: "Diversion", valorTotalDisponible: 0, porcentaje: 10 },
-      { nombre: "Inversion", valorTotalDisponible: 0, porcentaje: 10 },
-      { nombre: "Dar", valorTotalDisponible: 0, porcentaje: 5 },
-    ];
-  }
-}
-
-function ingresosLocalStorage() {
-  localStorage.setItem("ingresos", JSON.stringify(todoIngreso));
-}
-
-function gastosLocalStorage() {
-  localStorage.setItem("gastos", JSON.stringify(todoGasto));
-}
-
-function imprimirIngresoLocalStorage() {
-  let htmlListaIngreso = "";
-
-  todoIngreso = JSON.parse(localStorage.getItem("ingresos"));
-
-  if (todoIngreso === null) {
-    todoIngreso = [];
-  }
-
-  todoIngreso.map((valor, indice) => {
-    if (indice > 9) {
-      return;
-    }
-
-    htmlListaIngreso += `<p class="alert alert-success" role="alert"> Fecha : ${valor.fechaIngreso} Descripcion : ${valor.descIngreso}  Monto: $${valor.montoIngreso} `;
-  });
-
-  listaIngreso.innerHTML = htmlListaIngreso;
-}
-
-function imprimirGastosLocalStorage() {
-  let htmlListaGasto = "";
-
-  todoGasto = JSON.parse(localStorage.getItem("gastos"));
-
-  if (todoGasto === null) {
-    todoGasto = [];
-  }
-
-  todoGasto.map((valor, indice) => {
-    if (indice > 9) {
-      return;
-    }
-    htmlListaGasto += `<p  class="alert alert-danger" role="alert"> Fecha : ${
-      valor.fechaGasto
-    } Descripcion : ${valor.descGasto}  Monto: $${valor.montoGasto}  Jarron: ${
-      jarrones[valor.jarron].nombre
-    }</p> `;
-  });
-
-  listaGasto.innerHTML = htmlListaGasto;
-}
-
-function imprimirOption(valor, indice) {
-  jarron.innerHTML += `<option value="${indice}"> ${valor.nombre} </option>`;
-}
-
-function imprimirJarrones(valor, indice) {
-  contenedorJarrones.innerHTML += ` <div class="contenedorJarron">
-<div class="tituloJarron">
-<img class="imgJarron" src="img/j${indice}.png">
-  <h3>${valor.nombre}</h3>
-  
-</div>
-<div class="montoJarron">
-  <h3 id="valor${indice}">$${valor.valorTotalDisponible.toFixed(2)}</h3>
-</div>
-<div class="botonJarron">
-  <div>
-    <h3>${valor.porcentaje}%</h3>
-  </div>
-</div>
-</div> `;
-}
-
-function guardarIngreso(e) {
+const guardarIngreso = (e) => {
   e.preventDefault(); // hace que no se refresque la pagina cada vez que apretamos el submit del formulario
   let fechaIngreso = document.querySelector("#fechaIngreso").value;
   let montoIngreso = Number(document.querySelector("#montoIngreso").value);
   let descIngreso = document.querySelector("#descripcionIngreso").value;
-
   if (montoIngreso > 0) {
     todoIngreso.unshift({ fechaIngreso, montoIngreso, descIngreso });
-
     jarrones.map((valor, indice) => {
       valor.valorTotalDisponible =
         valor.valorTotalDisponible + (montoIngreso / 100) * valor.porcentaje;
-
       localStorage.setItem("jarrones", JSON.stringify(jarrones));
-
       let idvalor = document.querySelector(`#valor${indice}`);
       idvalor.innerHTML = `$ ${valor.valorTotalDisponible.toFixed(2)}`;
     });
@@ -138,9 +18,9 @@ function guardarIngreso(e) {
   } else {
     alert("El ingreso debe ser mayor a 0");
   }
-}
+};
 
-function guardarGasto(e) {
+const guardarGasto = (e) => {
   e.preventDefault(); // hace que no se refresque la pagina cada vez que apretamos el submit del formulario
   let fechaGasto = document.querySelector("#fechaGasto").value;
   let montoGasto = Number(document.querySelector("#montoGasto").value);
@@ -163,9 +43,113 @@ function guardarGasto(e) {
   } else {
     alert("Fondos insuficientes");
   }
-}
+};
 
-function imprimirIngresos() {
+const cargarPagina = () => {
+  jarronesLocalStorage();
+  jarrones.map(imprimirOption);
+  jarrones.map(imprimirJarrones);
+  imprimirIngresoLocalStorage();
+  imprimirGastosLocalStorage();
+};
+
+document
+  .querySelector("#formIngreso")
+  .addEventListener("submit", guardarIngreso); // Detecta click boton de ingresos y ejecuta la funcion guardarIngreso
+
+document.querySelector("#formGasto").addEventListener("submit", guardarGasto); // Detecta click boton de gastos y ejecuta la funcion guardarGasto
+
+window.addEventListener("load", cargarPagina); // Cuando carga la pagina, ejecuta las funciones imprimirOption y imprimirJarrones
+
+let jarrones = [
+  { nombre: "Necesidades", valorTotalDisponible: 0, porcentaje: 55 },
+  { nombre: "Educacion", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Ahorro", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Diversion", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Inversion", valorTotalDisponible: 0, porcentaje: 10 },
+  { nombre: "Dar", valorTotalDisponible: 0, porcentaje: 5 },
+];
+
+let todoIngreso = []; // Array con ingresos y descripciones
+let todoGasto = []; // Array con gastos y descripciones
+
+const jarronesLocalStorage = () => {
+  jarrones = JSON.parse(localStorage.getItem("jarrones"));
+
+  if (jarrones === null) {
+    jarrones = [
+      { nombre: "Necesidades", valorTotalDisponible: 0, porcentaje: 55 },
+      { nombre: "Educacion", valorTotalDisponible: 0, porcentaje: 10 },
+      { nombre: "Ahorro", valorTotalDisponible: 0, porcentaje: 10 },
+      { nombre: "Diversion", valorTotalDisponible: 0, porcentaje: 10 },
+      { nombre: "Inversion", valorTotalDisponible: 0, porcentaje: 10 },
+      { nombre: "Dar", valorTotalDisponible: 0, porcentaje: 5 },
+    ];
+  }
+};
+
+const ingresosLocalStorage = () =>
+  localStorage.setItem("ingresos", JSON.stringify(todoIngreso));
+
+const gastosLocalStorage = () =>
+  localStorage.setItem("gastos", JSON.stringify(todoGasto));
+
+const imprimirIngresoLocalStorage = () => {
+  let htmlListaIngreso = "";
+  todoIngreso = JSON.parse(localStorage.getItem("ingresos"));
+  if (todoIngreso === null) {
+    todoIngreso = [];
+  }
+  todoIngreso.map((valor, indice) => {
+    if (indice > 9) {
+      return;
+    }
+    htmlListaIngreso += `<p class="alert alert-success" role="alert"> Fecha : ${valor.fechaIngreso} Descripcion : ${valor.descIngreso}  Monto: $${valor.montoIngreso} `;
+  });
+  listaIngreso.innerHTML = htmlListaIngreso;
+};
+
+const imprimirGastosLocalStorage = () => {
+  let htmlListaGasto = "";
+  todoGasto = JSON.parse(localStorage.getItem("gastos"));
+  if (todoGasto === null) {
+    todoGasto = [];
+  }
+  todoGasto.map((valor, indice) => {
+    if (indice > 9) {
+      return;
+    }
+    htmlListaGasto += `<p  class="alert alert-danger" role="alert"> Fecha : ${
+      valor.fechaGasto
+    } Descripcion : ${valor.descGasto}  Monto: $${valor.montoGasto}  Jarron: ${
+      jarrones[valor.jarron].nombre
+    }</p> `;
+  });
+  listaGasto.innerHTML = htmlListaGasto;
+};
+
+const imprimirOption = (valor, indice) =>
+  (jarron.innerHTML += `<option value="${indice}"> ${valor.nombre} </option>`);
+
+const imprimirJarrones = (valor, indice) => {
+  contenedorJarrones.innerHTML += ` <div class="contenedorJarron">
+<div class="tituloJarron">
+<img class="imgJarron" src="img/j${indice}.png">
+  <h3>${valor.nombre}</h3>
+  
+</div>
+<div class="montoJarron">
+  <h3 id="valor${indice}">$${valor.valorTotalDisponible.toFixed(2)}</h3>
+</div>
+<div class="botonJarron">
+  <div>
+    <h3>${valor.porcentaje}%</h3>
+  </div>
+</div>
+</div> `;
+};
+
+const imprimirIngresos = () => {
   let htmlListaIngreso = "";
 
   todoIngreso.map((valor, indice) => {
@@ -177,9 +161,9 @@ function imprimirIngresos() {
   });
 
   listaIngreso.innerHTML = htmlListaIngreso;
-}
+};
 
-function imprimirGastos() {
+const imprimirGastos = () => {
   let htmlListaGasto = "";
   todoGasto.map((valor, indice) => {
     if (indice > 9) {
@@ -194,7 +178,7 @@ function imprimirGastos() {
   });
 
   listaGasto.innerHTML = htmlListaGasto;
-}
+};
 
 $("#agregarIngreso").on("click", function () {
   $(".ingreso").slideDown(100);
