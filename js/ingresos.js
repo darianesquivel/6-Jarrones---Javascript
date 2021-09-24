2a5061f42aa65435d1b988ddd395121cbd6bd470
Darian Esquivel <darianesquivelf@gmail.com>
2021-09-24 14:10:44 -0300

    seccion lista de ingresos

---
+++ js/ingresos.js
@@ -0,0 +1,20 @@
+const imprimirIngresos = () => {
+  let htmlListaIngreso = "";
+
+  todoIngreso.map((valor, indice) => {
+    htmlListaIngreso += `<p class="seccionIngresosTotal alert alert-secondary" role="alert">Fecha : ${valor.fechaIngreso} Descripcion : ${valor.descIngreso}  Monto: $${valor.montoIngreso} `;
+  });
+  seccionListaIngresos.innerHTML = htmlListaIngreso;
+};
+
+// trae los ingresos del local y los imprime en pantalla
+const imprimirIngresoLocalStorage = () => {
+  todoIngreso = JSON.parse(localStorage.getItem("ingresos"));
+  if (todoIngreso === null) {
+    seccionListaIngresos.innerHTML = `<p class="alert alert-secondary" role="alert"> NO REGISTRO INGRESOS`;
+    return;
+  }
+  imprimirIngresos();
+};
+
+imprimirIngresoLocalStorage();
